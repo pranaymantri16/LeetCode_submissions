9b91c294d1c77136103eb78993a79e0083a3655b
pranaymantri16 <147647648+pranaymantri16@users.noreply.github.com>
2025-01-11 21:17:05 +0530

    Time: 135 ms (75.68%) | Memory: 18.3 MB (28.83%) - LeetSync

--- 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.py
+++ 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.py
@@ -2,13 +2,13 @@ class Solution:
     def canConvertString(self, s: str, t: str, k: int) -> bool:
         if len(s) != len(t):
             return False
-        shift_count = defaultdict(int)
-        for char_s, char_t in zip(s, t):
-            shift = (ord(char_t) - ord(char_s) + 26) % 26
-            if shift > 0:
-                shift_count[shift] += 1
-        for shift, count in shift_count.items():
-            max_moves_needed = shift + 26 * (count - 1)
-            if max_moves_needed > k:
+        sc = defaultdict(int)
+        for cs, ct in zip(s, t):
+            s = (ord(ct) - ord(cs) + 26) % 26
+            if s > 0:
+                sc[s] += 1
+        for s, count in sc.items():
+            m = s + 26 * (count - 1)
+            if m > k:
                 return False
         return True
