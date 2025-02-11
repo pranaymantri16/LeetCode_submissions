0debb43f57bc2b34a75ef089210a5747fec5a35b
pranaymantri16 <147647648+pranaymantri16@users.noreply.github.com>
2025-02-11 10:34:10 +0530

    Time: 37 ms (70.58%) | Memory: 17.7 MB (50.96%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,18 +1,4 @@
 class Solution:
-    def clearDigits(self, s: str) -> str:
-        stack = []
-        for char in s:
-            if char.isdigit():
-                if stack:
-                    stack.pop()
-            else:
-                stack.append(char)
-        return "".join(stack)
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-        for num in nums:
-            result ^= num
-        return result
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
