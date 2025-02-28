0c49e909cbe7638fa864ce2bff8d4441d416aa57
pranaymantri16 <147647648+pranaymantri16@users.noreply.github.com>
2025-02-28 19:07:03 +0530

    Time: 48 ms (72.37%) | Memory: 51.9 MB (13.99%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val !== expected) {
+                throw new Error("Not Equal");
+            }
+            return true;
+        },
+        notToBe: function(expected) {
+            if (val === expected) {
+                throw new Error("Equal");
+            }
+            return true;
+        }
+    };
+};
