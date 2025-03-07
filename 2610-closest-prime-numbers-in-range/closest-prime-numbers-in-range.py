47f7d3e32b8081d3ac33febde456d8f2f03f5e26
pranaymantri16 <147647648+pranaymantri16@users.noreply.github.com>
2025-03-07 12:42:41 +0530

    Time: 1535 ms (52.50%) | Memory: 35.2 MB (9.38%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        LIMIT = right + 1
+        is_prime = [True] * LIMIT
+        is_prime[0] = is_prime[1] = False
+        
+        for i in range(2, int(LIMIT ** 0.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, LIMIT, i):
+                    is_prime[j] = False
+        primes = [num for num in range(left, right + 1) if is_prime[num]]
+        if len(primes) < 2:
+            return [-1, -1]
+        min_gap = float('inf')
+        result = [-1, -1]
+        for i in range(len(primes) - 1):
+            gap = primes[i + 1] - primes[i]
+            if gap < min_gap:
+                min_gap = gap
+                result = [primes[i], primes[i + 1]]
+        return result
